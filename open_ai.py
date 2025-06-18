efe432a49293081ac32592c847298d59011b43ed
Suyash5151 <148860936+Suyash5151@users.noreply.github.com>
2025-06-18 22:53:21 +0530

    Add files via upload

--- open_ai.py
+++ open_ai.py
@@ -4,21 +4,27 @@ import google.generativeai as genai
 
 api_key = st.secrets["GEMINI_API_KEY"]
 genai.configure(api_key=api_key)
-
+st.markdown("""
+    <style>
+    .stApp {
+        background-color: #e6ecf0;
+    }
+    </style>
+    """, unsafe_allow_html=True)
 
 
 
 genai.configure(api_key="AIzaSyDbh4GAj6DzApAqtUtfbRdAl7fQ71o8oZM")  
 
-model = genai.GenerativeModel("gemini-2.0-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 st.title("💰 Personal Finance Tip Generator")
 with st.form("finance_form"):
-    user_input = st.text_input("Describe your financial goal or situation:")
-    user_cost= st.number_input("What is the estimated cost to achieve this goal?",min_value=0, step=100)
-    user_salary = st.number_input("Enter your monthly salary:", min_value=0, step=100)
-    user_expenses = st.number_input("Enter your monthly expenses:", min_value=0, step=100)
-    user_investments = st.number_input("Enter your current monthly investments:", min_value=0, step=100)
+    user_input = st.text_input("Describe your financial goal (Ex. Buying a car):")
+    user_cost= st.number_input("What is the estimated cost to achieve this goal?",min_value=0,step=1000)
+    user_salary = st.number_input("Enter your monthly salary:", min_value=0, step=1000)
+    user_expenses = st.number_input("Enter your monthly expenses:", min_value=0, step=1000)
+    user_investments = st.number_input("Enter your current monthly investments:", min_value=0, step=1000)
     user_behaviour=st.selectbox("What is your investing risk profile",["Aggressive Investor","Balanced Investor","Conservative Investor"])
     submitted = st.form_submit_button("Submit")
 
@@ -34,13 +40,16 @@ else:
 if submitted:
     with st.spinner("Thinking..."):
         response = model.generate_content(
-            f"Give a personal finance tip for achieving this goal: {user_input}, "
+            f"Give a personal finance tip for achieving this goal: {user_input},with a cost of:{user_cost} "
             f"with a monthly salary of: {user_salary}, "
             f"monthly expenses: {user_expenses}, "
             f"current monthly investments: {user_investments}, "
             f"and an investing risk profile of: {user_behaviour}."
             f"Suggest mutual funds for:{user_risk}"
-            f"Dont provide key considerations,how to invest,important considerations "
+            f"Dont provide key considerations,how to invest,important considerations"
+            f"Provide detailed explaination on how your invested money will grow at a specific rate of interest of the mutual fund and how many years it will take to achieve the goal be specific about the price of the goal user has given"
+            f"Focus on the investments the user is making and not on the money that is left over that might be used for other purposes"
+            
         )
 
         st.success("Tip:")
